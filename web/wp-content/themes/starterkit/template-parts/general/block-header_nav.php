2ea9783932ded89d84754bc71441dce0969f158c
j.martin <j.martin@lonsdale.fr>
2022-06-14 18:55:01 +0200

    logo in icons

--- web/wp-content/themes/starterkit/template-parts/general/block-header_nav.php
+++ web/wp-content/themes/starterkit/template-parts/general/block-header_nav.php
@@ -1,11 +1,11 @@
 <?php
-    $options_fields = get_fields('options');
+$options_fields = get_fields('options');
 ?>
 
 <header id="header" role="banner">
     <div class="container">
         <a href="/" class="logo">
-            <img src="<?= THEME_URL ?>assets/img/logo.svg" alt="<?= get_bloginfo() ?>" width="49" height="49">
+            <?= icon('logo', 49, 49) ?>
         </a>
     </div>
 </header>
