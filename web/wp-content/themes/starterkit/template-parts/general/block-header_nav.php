52171ff9987ec444dc120e69356f2636ba89ab11
j.martin <j.martin@lonsdale.fr>
2022-06-21 12:04:37 +0200

    nav

--- web/wp-content/themes/starterkit/template-parts/general/block-header_nav.php
+++ web/wp-content/themes/starterkit/template-parts/general/block-header_nav.php
@@ -3,24 +3,25 @@ $options_fields = get_fields('options');
 ?>
 
 <header id="header" role="banner">
-    <div class="container header-content">
-        <a href="/" class="logo">
-            <?= icon('logo', 49, 49) ?>
-        </a>
 
-        <button id="btn-nav">Menu</button>
+    <a href="/" class="logo">
+        <?= icon('logo', 49, 49) ?>
+    </a>
 
+    <button id="btn-nav" aria-expanded="false" aria-controls="nav-panel">Menu</button>
+
+    <div id="nav-panel" aria-label="Access to navigation">
         <nav id="nav" role="navigation">
             <?php
             wp_nav_menu(array(
                 'container' => false,
                 'theme_location' => 'menu-header',
                 'menu_class'  => false,
-                'items_wrap' => '<ul>%3$s</ul>',
-                'walker' => new Walker_Nav_Menu()
+                'items_wrap' => '<ul class="nav-links">%3$s</ul>',
+                'walker' => new menu_header_Walker()
             ));
             ?>
         </nav>
-
     </div>
+
 </header>
